5aef12d248081f6dde0f7c1b559e4c760ac52f8c
Kyabetsu8931Ya31 <yuuki.20020217@gmail.com>
2022-08-03 17:33:05 +0900

    server

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+const PORT = process.env.PORT || 7000;
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(PORT,function(){
+    console.log('server listening. Port:'+PORT);
+});
+
+io.on('connection',function(socket){
+    let roomId = '';
+    socket.on('message',function(msg){
+        console.log('message:'+msg);
+        console.log('id:'+roomId);
+        io.to(roomId).emit('message',msg);
+    });
+
+    socket.on('join2room',(data) => {
+        roomId = data;
+        socket.join(roomId);
+        console.log('join:'+roomId);
+    });
+});
